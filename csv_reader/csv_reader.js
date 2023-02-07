c225345617991678b0a4e446be8b9f85052b9242
Zak <github@zakburrows.co.uk>
2023-02-07 15:53:30 +0000

    can read from local file. need to do client side?!

--- csv_reader/csv_reader.js
+++ csv_reader/csv_reader.js
@@ -2,9 +2,28 @@ document.write("Test")
 
 var string = "test, name\n1,zak"
 
-var count = 0; // cache the running count
-var result = Papa.parse(string, {
-    header: true
+function readSingleFile(e) {
+    var file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var contents = e.target.result;
+      displayContents(contents);
+    };
+    reader.readAsText(file);
+  }
+  
+  function displayContents(contents) {
+        var count = 0; // cache the running count
+        var result = Papa.parse(contents, {
+        header: true
 });
 
 console.log(result);
+  }
+  
+  document.getElementById('file-input')
+    .addEventListener('change', readSingleFile, false);
+
