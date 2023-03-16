c5d1321df2a7b39896f25a379f5e560d1573e6fe
v-Zak <77330725+v-Zak@users.noreply.github.com>
2023-03-16 19:21:16 +0000

    analysis by post added

--- app.py
+++ app.py
@@ -17,29 +17,18 @@ st.set_page_config(page_title='Facebook Sentimental Analysis',
 st.title('Facebook Sentiment Analysis')
 
 # display the results on the webpage 
+
 st.caption('This is the NLTK results of the Facebook comments')
 st.dataframe(na.sia_results)
 
 st.caption('These are the top most 50 common words')
 st.dataframe(na.frequency_results)
 
+st.caption('Word Cloud showing highest frequency words in the whole dataset')
 chart.word_cloud(na.frequency_results)
 
+st.caption('The overall sentiment of the dataset')
 chart.pie_chart(na.sia_results)
 
-
-def display_filtered_pie_chart():
-    # Get unique post_ids
-    unique_post_ids = na.dataframe["post_name"].unique()
-    selected_post_id = st.selectbox("Select a post:", unique_post_ids)
-
-    # Filter data and calculate sentiment
-    filtered_data = na.filter_by_post(selected_post_id).copy()
-    filtered_sia_results = na.calc_sentiment_filtered(filtered_data)
-
-    # Display pie chart
-    chart.pie_chart(filtered_sia_results, title=f'Sentiment for post {selected_post_id}')
-
-
-
-display_filtered_pie_chart()
+st.caption('The sentiment by post name')
+chart.display_filtered_pie_chart(na)
--- chart.py
+++ chart.py
@@ -2,19 +2,33 @@ import streamlit as st
 import plotly.express as px
 from wordcloud import WordCloud
 
+# show average sentiment of dataset in a pie chart
 def pie_chart(dataset, title='Overall Sentiment'):
+    # mean each sentiment type
     mean_negative = dataset["negative"].mean()
     mean_positive = dataset["positive"].mean()
     mean_neutral = dataset["neutral"].mean()
 
+    # create a pie chart using the means
     means = [mean_negative, mean_positive, mean_neutral]
     names = ['negative', 'positive', 'neutral']
-    fig = px.pie(values=means, names=names) # need to add colours to chart 
+    fig = px.pie(values=means, names=names)
 
     st.caption(title)
     st.plotly_chart(fig)
 
+# show the average sentiment of a specific post
+def display_filtered_pie_chart(analyzer):
+    # Get unique post_ids
+    unique_post_ids = analyzer.dataframe["post_name"].unique()
+    selected_post_id = st.selectbox("Select a post:", unique_post_ids)
 
+    # Filter data and calculate sentiment
+    filtered_data = analyzer.filter_by_post(selected_post_id).copy() #need to change to post name on drop down
+    filtered_sia_results = analyzer.calc_sentiment_filtered(filtered_data)
+
+    # Display pie chart
+    pie_chart(filtered_sia_results, title=f'Sentiment for post {selected_post_id}')
 
 def word_cloud(dataset):
     # Generating word cloud
