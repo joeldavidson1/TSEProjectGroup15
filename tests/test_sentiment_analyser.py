e62aa6a4aa77203ca07220a175c168fd2a646deb
Joel Davidson <joeldavo97@gmail.com>
2023-05-10 09:22:45 +0100

    added unit tests for test_sentiment_analyser.py

--- tests/test_sentiment_analyser.py
+++ tests/test_sentiment_analyser.py
@@ -1,4 +1,6 @@
 from sentiment_analyser import Sentiment_Analyser, calc_compound
+import pandas as pd
+import numpy
 import nltk
 
 
@@ -42,3 +44,43 @@ def test_calc_robert_sentiment_text() -> None:
     assert calculated_sample_results['negative'].iloc[0] == 0.004708835389465094
     assert calculated_sample_results['positive'].iloc[0] == 0.9415538311004639
     assert calculated_sample_results['neutral'].iloc[0] == 0.0537373423576355
+
+
+def test_calc_nltk_sentiment() -> None:
+    sia = Sentiment_Analyser()
+    sia_data = {'message': 'This is good sample text',
+                'from_post_id': '012_111'
+                }
+    sia_df = pd.DataFrame(sia_data, index=[0])
+    sia.dataframe = sia_df
+    calculated_sentiment = sia.calc_nltk_sentiment()
+
+    data = {'comment': 'This is good sample text',
+            'compound': 0.420000,
+            'negative': 0.000000,
+            'neutral': 0.580000,
+            'positive': 0.420000,
+            'from_post_id': '111'
+            }
+    df = pd.DataFrame(data, index=[0])
+    pd.testing.assert_frame_equal(calculated_sentiment, df)
+
+
+def test_calc_roberta_sentiment() -> None:
+    sia = Sentiment_Analyser()
+    sia_data = {'message': 'This is good sample text',
+                'from_post_id': '012_111'
+                }
+    sia_df = pd.DataFrame(sia_data, index=[0])
+    sia.dataframe = sia_df
+    calculated_sentiment = sia.calc_roberta_sentiment()
+
+    data = {'comment': 'This is good sample text',
+            'compound': numpy.float32(0.9368450045585632),
+            'negative': numpy.float32(0.004708835389465094),
+            'neutral': numpy.float32(0.0537373423576355),
+            'positive': numpy.float32(0.9415538311004639),
+            'from_post_id': '111'
+            }
+    df = pd.DataFrame(data, index=[0])
+    pd.testing.assert_frame_equal(calculated_sentiment, df)
